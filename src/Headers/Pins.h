487583728d17694453e71ae7c975c149cd03bb48
Gaston118 <gaston.capdevila@mi.unc.edu.ar>
2025-10-29 20:04:29 -0300

    Folder struct (Headers and Src)

---
+++ src/Headers/Pins.h
@@ -0,0 +1,13 @@
+/*
+ * Pins.h
+ *
+ *  Created on: 29 oct. 2025
+ *      Author: GastonCapdevila
+ */
+
+#ifndef HEADERS_PINS_H_
+#define HEADERS_PINS_H_
+
+void configPins(void);
+
+#endif /* HEADERS_PINS_H_ */
---
+++ src/Pins.c
@@ -0,0 +1,12 @@
+/*
+ * Pins.c
+ *
+ *  Created on: 29 oct. 2025
+ *      Author: GastonCapdevila
+ */
+
+#include "Headers/Pins.h"
+
+void configPins(void){
+	// Nothing
+}
